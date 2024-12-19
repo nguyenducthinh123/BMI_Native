0385ba01586b08bf4fd54c3298cf56c8190a81f6
Nguyễn Đức Thịnh <thinhnd111222@gmail.com>
2024-12-19 23:34:17 +0700

    them fadeIn, fadeOut cho ExplainScreen

--- ExplainScreen.js
+++ ExplainScreen.js
@@ -1,15 +1,60 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { useIsFocused, useNavigation } from '@react-navigation/native';
 
 export default function ExplainScreen() {
+  const fadeAnim = useState(new Animated.Value(0))[0];
+  const isFocused = useIsFocused();
+  const navigation = useNavigation();
+  const [isFadingOut, setIsFadingOut] = useState(false);
+
+  useEffect(() => {
+    if (isFocused) {
+      fadeIn();
+    }
+  }, [isFocused]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('beforeRemove', (e) => {
+      if (isFadingOut) {
+        e.preventDefault(); // Ngăn chặn hành động quay lại nếu đang fade out
+      } else {
+        e.preventDefault();
+        fadeOut(() => navigation.dispatch(e.data.action)); // Fade out trước khi thực hiện hành động quay lại
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation, isFadingOut]);
+
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = (callback) => {
+    setIsFadingOut(true);
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start(() => {
+      setIsFadingOut(false); // Đặt lại trạng thái
+      if (callback) callback();
+    });
+  };
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
       <Text style={styles.text}>BMI = w/h² (w in kg, h in meters)</Text>
       <Text style={styles.text}>&lt; 18.5 : Underweight</Text>
       <Text style={styles.text}>18.5 ÷ 25 : Normal</Text>
       <Text style={styles.text}>25 ÷ 30 : Overweight</Text>
       <Text style={styles.text}>≥ 30 : Obese</Text>
-    </View>
+    </Animated.View>
   );
 }
 
